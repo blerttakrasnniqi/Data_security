6e9ec2f1e66ce831029185a4044315d40eaf2340
Blina Retkoceri <117458349+blinaretkoceri@users.noreply.github.com>
2023-04-15 23:25:33 +0200

    Update Programimi_ne_algoritmet_per_enkriptim.py

--- Programimi_ne_algoritmet_per_enkriptim.py
+++ Programimi_ne_algoritmet_per_enkriptim.py
@@ -5,3 +5,11 @@ plaintext = "The quick brown fox jumps over the lazy dog"
 cyphertext = "012-011-007-018-024-003-020-019-013-004-009-015-002-006-009-023-024-001-022-014-009-021-007-004-012-011-007-016-005-008-017-009-010"
 
 book_file = "book.txt"
+
+# Read the book
+
+if not os.path.isfile(book_file):
+    print(f"Error: File '{book_file}' not found.")
+else:
+    with open(book_file, "r", encoding='utf-8') as f:
+        book = f.read()
