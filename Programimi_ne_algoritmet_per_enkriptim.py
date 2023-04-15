6d8c820593cf46f078721ffc0ed1bfab464ca16c
blerttakrasnniqi <129339285+blerttakrasnniqi@users.noreply.github.com>
2023-04-15 23:24:07 +0200

    Update Programimi_ne_algoritmet_per_enkriptim.py

--- Programimi_ne_algoritmet_per_enkriptim.py
+++ Programimi_ne_algoritmet_per_enkriptim.py
@@ -1,1 +1,7 @@
+import os
 
+plaintext = "The quick brown fox jumps over the lazy dog"
+
+cyphertext = "012-011-007-018-024-003-020-019-013-004-009-015-002-006-009-023-024-001-022-014-009-021-007-004-012-011-007-016-005-008-017-009-010"
+
+book_file = "book.txt"
